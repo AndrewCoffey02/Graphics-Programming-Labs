a9f81af4424f41336f4559cb635c866c49f22fa3
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-11-21 19:24:38 +0000

    Added up movement

--- Lab-7/Lab-7/Assets/SphereMovement.cs
+++ Lab-7/Lab-7/Assets/SphereMovement.cs
@@ -13,6 +13,11 @@ public class CubeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+            transform.Translate(Vector3.up);
+        }
         
     }
 }
