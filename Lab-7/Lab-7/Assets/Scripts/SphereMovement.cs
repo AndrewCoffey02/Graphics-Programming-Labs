0dbceba2c7b2b3c999a25c97b260576c804b508f
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-11-21 20:34:36 +0000

    added keybinds for sphere movement

--- Lab-7/Lab-7/Assets/Scripts/SphereMovement.cs
+++ Lab-7/Lab-7/Assets/Scripts/SphereMovement.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
+    //rigidbody component
     public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        //call the rigid component
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //make sphere jump vector
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Space key was pressed.");
             transform.Translate(Vector3.up);
         }
-
+        
+        //sphere movement across x ad z axis
         if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(Vector3.forward, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(Vector3.back, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector3.left, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector3.right, ForceMode.Force);
         }
         
     }
