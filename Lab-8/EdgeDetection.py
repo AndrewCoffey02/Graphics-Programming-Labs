34613d8ff6f0aadb08a2e220cb3ccdd76b88135e
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-07 18:21:20 +0000

    two images showing on one window

--- Lab-8/EdgeDetection.py
+++ Lab-8/EdgeDetection.py
@@ -3,13 +3,19 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('ATU.jpg',)
-cv2.imshow('Original', img)
 cv2.waitKey(0)
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-  
-cv2.imshow('Grayscale', gray_image) 
-cv2.waitKey(0)  
+cv2.waitKey(0)
+
+nrows = 2
+ncols = 1
+
+plt.subplot(nrows, ncols,1),plt.imshow(img, cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
+plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
+plt.show()
 
 
 
