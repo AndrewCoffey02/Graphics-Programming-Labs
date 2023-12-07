35f2b264d5e9cbc09a98d3662956e6ea8cfe29ce
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-07 16:06:13 +0000

    added grayscale

--- Lab-8/EdgeDetection.py
+++ Lab-8/EdgeDetection.py
@@ -6,7 +6,10 @@ img = cv2.imread('ATU.jpg',)
 cv2.imshow('Original', img)
 cv2.waitKey(0)
 
-
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+  
+cv2.imshow('Grayscale', gray_image) 
+cv2.waitKey(0)  
 
 
 
