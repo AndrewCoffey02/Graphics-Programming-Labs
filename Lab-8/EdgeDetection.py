6a6a0b6b868a7be15e11c24be6c34f89b3a82f7f
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-07 18:53:51 +0000

    added blur images

--- Lab-8/EdgeDetection.py
+++ Lab-8/EdgeDetection.py
@@ -2,20 +2,28 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+# variables for subplot
+nrows = 2
+ncols = 2
+
+# original figure
 img = cv2.imread('ATU.jpg',)
 cv2.waitKey(0)
 
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+# grayscale figure
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.waitKey(0)
 
-nrows = 2
-ncols = 1
+blur3 = cv2.GaussianBlur(img, (3,3), 0)
+blur13 = cv2.GaussianBlur(img,(13,13), 0)
 
-plt.subplot(nrows, ncols,1),plt.imshow(img, cmap = 'gray')
+# image dimensions and output
+plt.subplot(nrows, ncols, 1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
+plt.subplot(nrows, ncols, 2),plt.imshow(gray_image, cmap = 'gray')
 plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols, 3),plt.imshow(cv2.cvtColor(blur3, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('3x3 Blur'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols, 4),plt.imshow(cv2.cvtColor(blur13, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('13x13 Blur'), plt.xticks([]), plt.yticks([])
 plt.show()
-
-
-
