bdafeb5c09ca9884e54ade6e3726ac5777d7daf6
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-14 15:17:41 +0000

    added cornerharris and plotted to image

--- Lab-9/Lab9.py
+++ Lab-9/Lab9.py
@@ -5,12 +5,18 @@ import copy
 
 img = cv2.imread('ATU1.jpg', )
 
-imgHarris = copy.deepcopy(img)
-
 gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 dst = cv2.cornerHarris(gray_scale, 2, 3, 0.04)
 
-cv2.imshow('dst', dst)
+imgHarris = copy.deepcopy(img)
+
+threshold = 0.5
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(255, 0, 0),-1)
+
+cv2.imshow('dst', imgHarris)
 cv2.waitKey(0)
 
