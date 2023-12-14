2195456d66eab2a37f5e1cd121d2ecb726e0a45e
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-14 14:47:45 +0000

    added grayscale

--- Lab-9/Lab9.py
+++ Lab-9/Lab9.py
@@ -2,3 +2,10 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+img = cv2.imread('ATU1.jpg', )
+
+gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('gray_scale',gray_scale)
+cv2.waitKey(0)
+
