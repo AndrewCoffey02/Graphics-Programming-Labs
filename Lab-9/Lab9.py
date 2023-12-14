a4591a8ef86ab4f793336b90045f650a08ad0852
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-14 15:44:18 +0000

    added featured corners

--- Lab-9/Lab9.py
+++ Lab-9/Lab9.py
@@ -9,14 +9,18 @@ gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 dst = cv2.cornerHarris(gray_scale, 2, 3, 0.04)
 
-imgHarris = copy.deepcopy(img)
+imgHarris = copy.deepcopy(gray_scale)
 
 threshold = 0.5
 for i in range(len(dst)):
     for j in range(len(dst[i])):
         if dst[i][j] > (threshold*dst.max()):
-            cv2.circle(imgHarris,(j,i),3,(255, 0, 0),-1)
+            cv2.circle(imgHarris,(j,i),3,(0, 0, 0),-1)
 
-cv2.imshow('dst', imgHarris)
+corners = cv2.goodFeaturesToTrack(gray_scale,25,0.01,10) 
+
+imgShiTomasi = copy.deepcopy(corners)
+
+cv2.imshow('dst', imgShiTomasi)
 cv2.waitKey(0)
 
