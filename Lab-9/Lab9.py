5cecce3ca24b842e63cd3032b6fb1f2bd4a6fd6c
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-14 15:03:24 +0000

    added deepcopy

--- Lab-9/Lab9.py
+++ Lab-9/Lab9.py
@@ -1,11 +1,16 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import copy
 
 img = cv2.imread('ATU1.jpg', )
 
+imgHarris = copy.deepcopy(img)
+
 gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('gray_scale',gray_scale)
+dst = cv2.cornerHarris(gray_scale, 2, 3, 0.04)
+
+cv2.imshow('dst', dst)
 cv2.waitKey(0)
 
