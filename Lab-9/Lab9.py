58b9c44456e9b8de5a3d3e3ba19a0dfb05d605af
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-19 11:18:16 +0000

    added prohobition symbol + spin

--- Lab-9/Lab9.py
+++ Lab-9/Lab9.py
@@ -11,16 +11,16 @@ dst = cv2.cornerHarris(gray_scale, 2, 3, 0.04)
 
 imgHarris = copy.deepcopy(gray_scale)
 
-threshold = 0.5
+threshold = 0.2
 for i in range(len(dst)):
     for j in range(len(dst[i])):
         if dst[i][j] > (threshold*dst.max()):
             cv2.circle(imgHarris,(j,i),3,(0, 0, 0),-1)
 
-corners = cv2.goodFeaturesToTrack(gray_scale,25,0.01,10) 
+corners = cv2.goodFeaturesToTrack(dst,25,0.01,10)
 
 imgShiTomasi = copy.deepcopy(corners)
 
-cv2.imshow('dst', imgShiTomasi)
+cv2.imshow('dst', corners)
 cv2.waitKey(0)
 
