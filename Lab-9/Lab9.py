edd584e2c175bf3efe8ccf0e048823d54c68afc6
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-12-19 11:35:17 +0000

    comments

--- Lab-9/Lab9.py
+++ Lab-9/Lab9.py
@@ -3,21 +3,27 @@ import numpy as np
 from matplotlib import pyplot as plt
 import copy
 
+# main image
 img = cv2.imread('ATU1.jpg', )
 
+# gray scaled image
 gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# corner harris image
 dst = cv2.cornerHarris(gray_scale, 2, 3, 0.04)
 
+# changed to deep copy
 imgHarris = copy.deepcopy(gray_scale)
 
+# circles added to cornerHarris
 threshold = 0.2
 for i in range(len(dst)):
     for j in range(len(dst[i])):
         if dst[i][j] > (threshold*dst.max()):
             cv2.circle(imgHarris,(j,i),3,(0, 0, 0),-1)
 
-corners = cv2.goodFeaturesToTrack(dst,25,0.01,10)
+# Shi Tomasi algorithim
+corners = cv2.goodFeaturesToTrack(gray_scale,2,0.01,10)
 
 imgShiTomasi = copy.deepcopy(corners)
 
